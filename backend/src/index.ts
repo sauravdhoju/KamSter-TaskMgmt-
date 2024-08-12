002d51fce5949954e2e48eff1726c0af98a7e39f
nimeshakya <nimeshakyabtw@gmail.com>
2024-08-12 15:20:20 +0545

    Showcasing and testing the project.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,8 @@ const app = express();
 app.use(
     cors({
         credentials: true,
+        origin: 'http://localhost:5173',
+        optionsSuccessStatus: 200,
     })
 );
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,28 @@
-import { Box, Button } from '@chakra-ui/react';
+import axios from 'axios';
+import { Box, Button, Heading } from '@chakra-ui/react';
 
 import './Home.scss';
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:8080';
 
 const Home = () => {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios
+            .get(`${baseUrl}/`, { withCredentials: true })
+            .then((res) => {
+                setMessage(res.data.name);
+                console.log(res);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <Box>
             <Button variant={'link'}>click me</Button>
+            <Heading>{message === '' ? 'Empty message!' : message}</Heading>
         </Box>
     );
 };
