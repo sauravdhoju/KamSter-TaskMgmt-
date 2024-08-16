35c2e3721bfa74f81b1f597dae15549e6eaf6a3d
jeenakarmi <jinanakarmi@gmail.com>
2024-08-16 22:43:27 +0545

    Added for side_bar in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex } from '@chakra-ui/react';
+import 'boxicons/css/boxicons.min.css';
 
 import NavBar from './components/NavBar/NavBar';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+
+import Sidebar from './pages/Sidebar/Sidebar';
+// import Calender from './pages/Calender/Calender';
+// import Kanban from './pages/Kanban/Kanban';
 
 import './App.scss';
 
@@ -13,13 +18,28 @@ const App = () => {
     return ( 
         <Box className='app'>
             <NavBar />
+
+            <Flex>
+            {/*Sidebar here */}
+            <Sidebar />
+
+            <Box flex="1">
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/auth/login' element={<Login />} />
+            
+                {/* <Route path="/calendar" element={<Calender />} />
+                <Route path="/kanban" element={<Kanban />} /> */}
+     
             </Routes>
+            </Box>
+            </Flex>
         </Box>
+    
     );
 };
+
+
 
 export default App;
 
