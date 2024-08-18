18c61c2554c6f6e60b357526cef0305d43ca52e4
kiranshah59 <kiranshah9182@gmail.com>
2024-08-18 13:28:16 -0700

    Creating home page.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import { Box, Button, Heading } from '@chakra-ui/react';
-
 import './Home.scss';
 import { useEffect, useState } from 'react';
 
 const baseUrl = 'http://localhost:8080';
 
-const Home = () => {
-    const [message, setMessage] = useState('');
+const Home: React.FC = () => {
+    const [message, setMessage] = useState<string>('');
 
     useEffect(() => {
         axios
-            .get(`${baseUrl}/whatistoday`, { withCredentials: true })
+            .get<{ message: string }>(`${baseUrl}/whatistoday`, { withCredentials: true })
             .then((res) => {
                 setMessage(res.data.message);
                 console.log(res);
@@ -21,8 +20,18 @@ const Home = () => {
 
     return (
         <Box>
-            <Button variant={'link'}>click me</Button>
-            <Heading>{message === '' ? 'Empty message!' : message}</Heading>
+            <Box className="upper-part">
+                <Heading mt={4}>{message === '' ? 'Hello,!' : message}</Heading>
+                <Heading mt={4}>{message === '' ? 'Good morning!' : message}</Heading>
+                <Box className="task-summary">
+                    <a href="#!">10 tasks completed,5 pending</a>
+                    </Box>
+                    </Box>
+                <input className="search-input" type="text" placeholder="Search for anything..." />
+                
+                    
+                
+            
         </Box>
     );
 };
