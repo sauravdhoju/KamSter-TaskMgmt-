0fae56e2e48cfc7b8cf43af9b7d8941789a254e8
kiranshah59 <kiranshah9182@gmail.com>
2024-09-02 12:25:19 -0700

    Home page completed

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Box, Button, Heading } from '@chakra-ui/react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import './Home.scss';
 import { useEffect, useState } from 'react';
 
@@ -24,14 +24,28 @@ const Home: React.FC = () => {
                 <Heading mt={4}>{message === '' ? 'Hello,!' : message}</Heading>
                 <Heading mt={4}>{message === '' ? 'Good morning!' : message}</Heading>
                 <Box className="task-summary">
-                    <a href="#!">10 tasks completed,5 pending</a>
-                    </Box>
-                    </Box>
+                    <a href="#!">10 tasks completed, 5 pending</a>
+                </Box>
+                </Box>
                 <input className="search-input" type="text" placeholder="Search for anything..." />
-                
-                    
-                
             
+            
+            <Box className="button-section">
+                <Button className="my-list-button" onClick={() => console.log('My List clicked')}>
+                    <span className="star-sign">★</span> My List
+                </Button>
+                <Button className="new-list-button" onClick={() => console.log('New List clicked')}>
+                    <span className="plus-sign">+</span> New List
+                </Button>
+            </Box>
+            <hr className="divider" />
+            <Text className="hire-me-text">
+                <span className="circle-sign">o</span> Hire Me for Designer <span className= "star-sign">★</span>
+            </Text>
+             {/* Positioned at the bottom-right corner */}
+            <Button className="plus-button" onClick={() => console.log('Add New List')}>
+                <span className="plus-sign">+</span>
+            </Button>
         </Box>
     );
 };
