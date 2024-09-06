bca38a65521175bb9c4a8596ebf845976d7babe3
nimeshakya <nimeshakyabtw@gmail.com>
2024-09-06 14:55:42 +0545

    Register page created.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Box, Heading } from '@chakra-ui/react';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import MyAccount from './pages/MyAccount/MyAccount';
 
 import './App.scss';
@@ -14,6 +15,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
                 <Route path='/my-account' element={<MyAccount />} />
             </Routes>
         </Box>
