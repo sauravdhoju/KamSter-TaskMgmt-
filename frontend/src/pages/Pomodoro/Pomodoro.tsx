b9cda48e2185022609f29540ecbab0d1c82a984a
nimeshakya <nimeshakyabtw@gmail.com>
2024-10-06 12:55:36 +0545

    Pomodoro set time action btns working.

--- frontend/src/pages/Pomodoro/Pomodoro.tsx
+++ frontend/src/pages/Pomodoro/Pomodoro.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
 
 import Icon from '../../components/Icon/Icon';
@@ -10,6 +10,64 @@ import './Pomodoro.scss';
 
 const Pomodoro = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [breakTime, setBreakTime] = useState(300000);
+    const [sessionTime, setSessionTime] = useState(1500000);
+    const [sessionType, setSessionType] = useState<'break' | 'session'>(
+        'session'
+    );
+
+    const [sessionCountDownString, setSessionCountDownString] =
+        useState('25:00');
+    const [breakCountDownString, setBreakCountDownString] = useState('05:00');
+
+    const getTimeString = (time: Date) => {
+        const min = time.getMinutes();
+        const sec = time.getSeconds();
+        const timeStr = `${min < 10 ? '0' + min : min}:${
+            sec < 10 ? '0' + sec : sec
+        }`;
+        return timeStr;
+    };
+
+    useEffect(() => {}, [breakTime, sessionTime]);
+
+    const handleTimerIncreaseDecrease = (
+        type: 'break' | 'session',
+        incDec: 'increase' | 'decrease'
+    ) => {
+        const time = new Date();
+        time.setHours(0, 0, 0, 0);
+        if (type === 'break') {
+            if (incDec === 'increase') {
+                if (breakTime + 60000 > 1800000) return;
+                setBreakTime(breakTime + 60000);
+                time.setMilliseconds(breakTime + 60000);
+
+                setBreakCountDownString(getTimeString(time));
+            } else if (incDec === 'decrease') {
+                if (breakTime - 60000 <= 0) return;
+                setBreakTime(breakTime - 60000);
+                time.setMilliseconds(breakTime - 60000);
+
+                setBreakCountDownString(getTimeString(time));
+            }
+        } else if (type === 'session') {
+            if (incDec === 'increase') {
+                if (sessionTime + 60000 >= 3600000) return;
+                setSessionTime(sessionTime + 60000);
+                time.setMilliseconds(sessionTime + 60000);
+
+                setSessionCountDownString(getTimeString(time));
+            } else if (incDec === 'decrease') {
+                if (sessionTime - 60000 <= 0) return;
+                setSessionTime(sessionTime - 60000);
+                time.setMilliseconds(sessionTime - 60000);
+
+                setSessionCountDownString(getTimeString(time));
+            }
+        }
+    };
+
     return (
         <Flex
             className='pomodoro-page'
@@ -37,14 +95,20 @@ const Pomodoro = () => {
                 <HeaderGreet />
                 <Box
                     className='pomodoro-timer'
-                    bgColor={'red'}
+                    width={'330px'}
                     justifySelf={'center'}
                     marginBottom={'85px'}
                     padding={'24px'}
                 >
                     <Box className='timer-screen'>
-                        <Text className='session-name'>BREAK</Text>
-                        <Text className='session-countdown'>05:00</Text>
+                        <Text className='session-name'>
+                            {sessionType === 'break' ? 'BREAK' : 'SESSION'}
+                        </Text>
+                        <Text className='session-countdown'>
+                            {sessionType === 'break'
+                                ? breakCountDownString
+                                : sessionCountDownString}
+                        </Text>
                     </Box>
                     <Grid
                         gridTemplateRows={'min-content min-content'}
@@ -66,14 +130,34 @@ const Pomodoro = () => {
                         </GridItem>
                         <GridItem className='set-action-btn set-break'>
                             <Box className='setter-container'>
-                                <Box className='decrease-increase-btn'>
+                                <Box
+                                    className='decrease-increase-btn'
+                                    onClick={() =>
+                                        handleTimerIncreaseDecrease(
+                                            'break',
+                                            'decrease'
+                                        )
+                                    }
+                                >
                                     <Icon
                                         name='bx-minus'
                                         className='inc-dec-icon'
                                     />
                                 </Box>
-                                <Text>5</Text>
-                                <Box className='decrease-increase-btn'>
+                                <Text>
+                                    {breakTime / 60000 < 10
+                                        ? '0' + breakTime / 60000
+                                        : breakTime / 60000}
+                                </Text>
+                                <Box
+                                    className='decrease-increase-btn'
+                                    onClick={() =>
+                                        handleTimerIncreaseDecrease(
+                                            'break',
+                                            'increase'
+                                        )
+                                    }
+                                >
                                     <Icon
                                         name='bx-plus'
                                         className='inc-dec-icon'
@@ -84,14 +168,30 @@ const Pomodoro = () => {
                         </GridItem>
                         <GridItem className='set-action-btn set-session'>
                             <Box className='setter-container'>
-                                <Box className='decrease-increase-btn'>
+                                <Box
+                                    className='decrease-increase-btn'
+                                    onClick={() =>
+                                        handleTimerIncreaseDecrease(
+                                            'session',
+                                            'decrease'
+                                        )
+                                    }
+                                >
                                     <Icon
                                         name='bx-minus'
                                         className='inc-dec-icon'
                                     />
                                 </Box>
-                                <Text>25</Text>
-                                <Box className='decrease-increase-btn'>
+                                <Text>{sessionTime / 60000}</Text>
+                                <Box
+                                    className='decrease-increase-btn'
+                                    onClick={() =>
+                                        handleTimerIncreaseDecrease(
+                                            'session',
+                                            'increase'
+                                        )
+                                    }
+                                >
                                     <Icon
                                         name='bx-plus'
                                         className='inc-dec-icon'
