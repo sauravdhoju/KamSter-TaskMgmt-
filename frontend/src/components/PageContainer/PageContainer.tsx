cd2596e878988167a76678f81287dc05ea5ad394
nimeshakya <nimeshakyabtw@gmail.com>
2024-11-27 20:08:42 +0545

    Used grid container.

--- frontend/src/components/PageContainer/PageContainer.tsx
+++ frontend/src/components/PageContainer/PageContainer.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 
 import Sidebar from '../Sidebar/Sidebar';
 import HeaderRenderer from '../HeaderRenderer/HeaderRenderer';
@@ -12,28 +12,34 @@ const PageContainer: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
             width={'100%'}
             height={'100vh'}
             minH={'100vh'}
+            maxH={'100vh'}
             flexDir={'row'}
         >
             <Sidebar
                 isSideBarOpen={isSidebarOpen}
                 setIsSideBarOpen={setIsSidebarOpen}
             />
-            <Box
+            <Grid
                 className='page-content-container'
                 width={'100%'}
                 maxW={'1920px'}
                 minH={'100%'}
                 flexGrow={1}
+                maxH={'100%'}
                 paddingX={'20px'}
-                display={'flex'}
-                flexDir={'column'}
+                gridTemplateColumns={'1fr'}
+                gridTemplateRows={'min-content 1fr'}
             >
-                <HeaderRenderer
-                    isSideBarOpen={isSidebarOpen}
-                    setIsSideBarOpen={setIsSidebarOpen}
-                />
-                {children}
-            </Box>
+                <GridItem>
+                    <HeaderRenderer
+                        isSideBarOpen={isSidebarOpen}
+                        setIsSideBarOpen={setIsSidebarOpen}
+                    />
+                </GridItem>
+                <GridItem maxH={'100%'} overflowY={'auto'}>
+                    {children}
+                </GridItem>
+            </Grid>
         </Flex>
     );
 };
