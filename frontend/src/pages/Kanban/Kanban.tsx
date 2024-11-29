58aee439a84dce8d2e788fea4bf7156d1c88a97a
jeenakarmi <jinanakarmi@gmail.com>
2024-11-29 12:09:54 +0545

    Boards can be added

--- frontend/src/pages/Kanban/Kanban.tsx
+++ frontend/src/pages/Kanban/Kanban.tsx
@@ -1,32 +1,55 @@
-import React from 'react';
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Flex, Heading, Text} from '@chakra-ui/react';
+import Icon from '../../components/Icon/Icon';
 import './Kanban.scss';
 import PageContainer from '../../components/PageContainer/PageContainer';
 
 const Kanban = () => {
-  const columns = {
-    todo: 'To Do',
-    doing: 'Doing',
-    complete: 'Complete',
+  const [columns, setColumns] = useState([
+    { id: 'todo', name: 'To Do' },
+    { id: 'doing', name: 'Doing' },
+    { id: 'done', name: 'Done' },
+  ]);
+
+  // Function to add a new board
+  const addBoard = () => {
+    const newColumnId = `new-board-${columns.length}`;
+    setColumns([...columns, { id: newColumnId, name: `New Board ${columns.length + 1}` }]);
   };
 
   return (
     <PageContainer>
-    <Flex className="kanban-board" gap="2" p="4">
-      {Object.entries(columns).map(([columnId, columnName]) => (
-        <Box key={columnId} className="column">
-          <Flex >
-            <Heading size="sm">{columnName}</Heading>
-          </Flex>
-          <Box className="add-task" mb="4">
-            <Text as="span">Add a task</Text>
+      <Flex className="kanban-board">
+        {columns.map((column) => (
+          <Box key={column.id} className="column">
+            <Heading size="sm" className="column-header">
+              {column.name}
+            </Heading>
+            <Box className="add-task">
+              <Text>Add a task</Text>
+            </Box>
+            <Box className="tasks">
+              {/* Empty task list for now */}
+            </Box>
           </Box>
-          <Box className="tasks">
-            {/* Task list*/}
-          </Box>
-        </Box>
-      ))}
-    </Flex>
+        ))}
+        {/* Add Board Button */}
+        <Box
+          as="button"
+          onClick={addBoard}
+          style={{
+            border: 'none',
+            background: 'none',
+            padding: '0',
+            cursor: 'pointer',
+          }}
+        >
+        <Icon
+          name='bx-plus-circle'
+          className="add-board-icon"
+        />
+      </Box>
+      </Flex>
     </PageContainer>
   );
 };
