76d9bb3c4d826ac444f336f7376d1d185cde7792
nimeshakya <nimeshakyabtw@gmail.com>
2024-11-30 16:17:24 +0545

    Added tasks page link to sidebar.

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -27,6 +27,10 @@ const Sidebar = ({ isSideBarOpen, setIsSideBarOpen }: SidebarTypes) => {
             icon: 'bx-home-alt-2',
         },
         {
+            name: 'tasks',
+            icon: 'bx-task',
+        },
+        {
             name: 'calendar',
             icon: 'bx-calendar',
         },
