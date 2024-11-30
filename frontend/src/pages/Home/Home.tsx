5ef13e19e480abed328479191faa38c98a213771
nimeshakya <nimeshakyabtw@gmail.com>
2024-11-30 15:48:14 +0545

    Fixed home page.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,15 +2,17 @@ import axios from 'axios';
 import { Image, Box } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import HeaderGreet from '../../components/HeaderGreet/HeaderGreet';
+import TasksList from '../../components/TasksList/TasksList';
 import './Home.scss';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import taskPhoto from '../../pages/Home/picture.jpg';
 
-
 const baseUrl = 'http://localhost:8080';
 
 const Home: React.FC = () => {
-    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+    const [currentTime, setCurrentTime] = useState(
+        new Date().toLocaleTimeString()
+    );
     const [currentDate, setCurrentDate] = useState(new Date());
 
     const totalTasks = 32;
@@ -20,7 +22,9 @@ const Home: React.FC = () => {
 
     useEffect(() => {
         axios
-            .get<{ message: string }>(`${baseUrl}/whatistoday`, { withCredentials: true })
+            .get<{ message: string }>(`${baseUrl}/whatistoday`, {
+                withCredentials: true,
+            })
             .then((res) => console.log(res.data.message))
             .catch((err) => console.log(err));
     }, []);
@@ -37,7 +41,7 @@ const Home: React.FC = () => {
         const midnight = new Date();
         midnight.setHours(24, 0, 0, 0);
         const timeToMidnight = midnight.getTime() - new Date().getTime();
-        
+
         const dateTimer = setTimeout(() => {
             updateDate();
             setInterval(updateDate, 24 * 60 * 60 * 1000);
@@ -49,84 +53,103 @@ const Home: React.FC = () => {
         };
     }, []);
 
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
     const formattedDate = currentDate.toLocaleDateString(undefined);
 
     return (
         <PageContainer>
-        <div className="home-container">
-            <section className="task-section">
-                <Box className="task-header" position={'relative'}>
-                <Box className='task-picture' position={'absolute'} right={0} bottom={0} zIndex={'99'}>
-                    <Image
-                        src={taskPhoto}
-                        alt='task-photo'
-                    />
-                </Box>
-                    <div className="task-text">
-                        <h2>Today Task</h2>
-                        <p>Check your daily tasks and schedules</p>
-                    </div>
-                    <div className="buttons">
-                        <button className="schedule-btn">Today's Schedule</button>
-                        <button className="add-btn">Add Task</button>
-                    </div>
-                </div>
-                <Box className='task-picture'>
-                    <Image
-                        src={taskPhoto}
-                        alt='task-photo'
-                    />
-                </Box>
-                
-                <div className="task-status-container">
-                    <div className="task-item">
-                        <p>Total Tasks</p>
-                        {/* <p className="all-label">All</p> */}
-                        <h3>{totalTasks}</h3>
-                        {/* <span className="percent-change">10% ↑</span> */}
-                    </div>
-                    <div className="task-item">
-                        <p>Ongoing Tasks</p>
-                        {/* <p className="all-label">All</p> */}
-                        <h3>{ongoingTasks}</h3>
-                    </div>
-                    <div className="task-item">
-                        <p>Completed Tasks</p>
-                        {/* <p className="all-label">All</p> */}
-                        <h3>{completedTasks}</h3>
-                    </div>
-                    <div className="task-item">
-                        <p>Overdue Tasks</p>
-                        {/* <p className="all-label">All</p> */}
-                        <h3>{overdueTasks}</h3>
-                    </div>
-                </div>
+            <div className='home-container'>
+                <section className='task-section'>
+                    <Box className='task-header' position={'relative'}>
+                        <Box
+                            className='task-picture'
+                            position={'absolute'}
+                            right={0}
+                            bottom={0}
+                            zIndex={'99'}
+                        >
+                            <Image
+                                src={taskPhoto}
+                                alt='task-photo'
+                                className='tasker-img'
+                            />
+                        </Box>
+                        <Box className='task-header-text-container'>
+                            <Box className='task-text'>
+                                <h2>Today Task</h2>
+                                <p>Check your daily tasks and schedules</p>
+                            </Box>
+                            <Box className='buttons'>
+                                <button className='schedule-btn'>
+                                    Today's Schedule
+                                </button>
+                                <button className='add-btn'>Add Task</button>
+                            </Box>
+                        </Box>
+                    </Box>
 
-                <div className="future-container">
-                <div className="upcoming-tasks">
-                    <h3>Upcoming Tasks: </h3>
-                </div>
-                
-                <div className="calendar">
-                    <h4>{currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</h4>
-                    <div className="calendar-grid">
-                        <div className="day">Sun</div>
-                        <div className="day">Mon</div>
-                        <div className="day">Tue</div>
-                        <div className="day">Wed</div>
-                        <div className="day">Thu</div>
-                        <div className="day">Fri</div>
-                        <div className="day">Sat</div>
-                        {[...Array(31)].map((_, i) => (
-                            <div key={i} className="date">
-                                {i + 1}
-                            </div>
-                        ))}
+                    <div className='task-status-container'>
+                        <div className='task-item'>
+                            <p>Total Tasks</p>
+                            {/* <p className="all-label">All</p> */}
+                            <h3>{totalTasks}</h3>
+                            {/* <span className="percent-change">10% ↑</span> */}
+                        </div>
+                        <div className='task-item'>
+                            <p>Ongoing Tasks</p>
+                            {/* <p className="all-label">All</p> */}
+                            <h3>{ongoingTasks}</h3>
+                        </div>
+                        <div className='task-item'>
+                            <p>Completed Tasks</p>
+                            {/* <p className="all-label">All</p> */}
+                            <h3>{completedTasks}</h3>
+                        </div>
+                        <div className='task-item'>
+                            <p>Overdue Tasks</p>
+                            {/* <p className="all-label">All</p> */}
+                            <h3>{overdueTasks}</h3>
+                        </div>
                     </div>
-                </div></div>
-            </section>
-        </div>
+
+                    <div className='future-container'>
+                        {/* <div className='upcoming-tasks'>
+                            <h3>Upcoming Tasks: </h3>
+                        </div> */}
+                        <Box>
+                            <TasksList />
+                        </Box>
+
+                        <Box className='calendar'>
+                            <h4>
+                                {currentDate.toLocaleString('default', {
+                                    month: 'long',
+                                    year: 'numeric',
+                                })}
+                            </h4>
+                            <div className='calendar-grid'>
+                                <div className='day'>Sun</div>
+                                <div className='day'>Mon</div>
+                                <div className='day'>Tue</div>
+                                <div className='day'>Wed</div>
+                                <div className='day'>Thu</div>
+                                <div className='day'>Fri</div>
+                                <div className='day'>Sat</div>
+                                {[...Array(31)].map((_, i) => (
+                                    <div key={i} className='date'>
+                                        {i + 1}
+                                    </div>
+                                ))}
+                            </div>
+                        </Box>
+                    </div>
+                </section>
+            </div>
         </PageContainer>
     );
 };
