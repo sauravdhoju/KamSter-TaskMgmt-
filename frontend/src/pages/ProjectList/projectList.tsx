e7c61813823145917b173598624f6958226060c2
nimeshakya <nimeshakyabtw@gmail.com>
2025-01-30 18:09:52 +0545

    Backend join for post and get

--- frontend/src/pages/ProjectList/projectList.tsx
+++ frontend/src/pages/ProjectList/projectList.tsx
@@ -1,40 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { 
-    Box, Heading, Button, Textarea, Input, Modal, ModalOverlay, ModalContent, 
-    ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Text, Flex
+import {
+    Box,
+    Heading,
+    Button,
+    Textarea,
+    Input,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    useDisclosure,
+    Text,
+    Flex,
 } from '@chakra-ui/react';
 
+import { useBackendAPIContext } from '../../contexts/BackendAPIContext/BackendAPIContext';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import Icon from '../../components/Icon/Icon';
 import './projectList.scss';
 
 type Project = {
-    id: string;
+    _id: string;
     name: string;
     description?: string; // optional
-  };
+    created_at: Date;
+    udpated_at: Date;
+    admin_id: string;
+};
 
 const ProjectList = () => {
+    const { client } = useBackendAPIContext();
     const [projects, setProjects] = useState<Project[]>([]);
     const [newProjectName, setNewProjectName] = useState('');
     const [newProjectDescription, setNewProjectDescription] = useState('');
     const [editingProject, setEditingProject] = useState<Project | null>(null);
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const { isOpen: isEditOpen, onOpen: onEditOpen, onClose: onEditClose } = useDisclosure();
+    const {
+        isOpen: isEditOpen,
+        onOpen: onEditOpen,
+        onClose: onEditClose,
+    } = useDisclosure();
     const navigate = useNavigate();
 
-    const addProject = () => {
-        if (newProjectName.trim()) {
+    const addProject = async () => {
+        try {
             const newProject = {
                 id: (projects.length + 1).toString(),
                 name: newProjectName,
                 description: newProjectDescription,
             };
-            setProjects([...projects, newProject]);
-            setNewProjectName('');
-            setNewProjectDescription('');
-            onClose();
+            const response = await client.post('/project/create', newProject);
+            const data = await response.data;
+            if (data) {
+                fetchProjects();
+                setNewProjectName('');
+                setNewProjectDescription('');
+                onClose();
+            }
+        } catch (error) {
+            console.error(error);
         }
     };
 
@@ -49,7 +77,7 @@ const ProjectList = () => {
             onEditClose();
         }
     };
-    
+
     const deleteProject = (id: string) => {
         setProjects((prevProjects) =>
             prevProjects.filter((project) => project.id !== id)
@@ -60,66 +88,113 @@ const ProjectList = () => {
         navigate(`/kanban/${projectName}`);
     };
 
+    const fetchProjects = async () => {
+        try {
+            const response = await client.get('/project/get');
+            const data = await response.data;
+            if (data) setProjects(data.projects);
+            else throw new Error('Projects not found!');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
     return (
         <PageContainer>
             <Box>
-                <Heading as="h1" size="lg" mb={5} textAlign="center">
+                <Heading as='h1' size='lg' mb={5} textAlign='center'>
                     Your Projects
                 </Heading>
 
                 {/* Display projects */}
-                <Box  display="grid" gridTemplateColumns={{ base: '1fr', md: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)' }} gap={6}>
+                <Box
+                    display='grid'
+                    gridTemplateColumns={{
+                        base: '1fr',
+                        md: 'repeat(2, 1fr)',
+                        lg: 'repeat(4, 1fr)',
+                    }}
+                    gap={6}
+                >
                     {projects.length > 0 ? (
                         projects.map((project) => (
                             <Box
-                                key={project.id} 
-                                p={4} 
-                                bg="white" 
-                                borderRadius="lg" 
-                                boxShadow="md"   
-                                onDoubleClick={() => handleDoubleClick(project.name)} // Navigate on double-click
+                                key={project.id}
+                                p={4}
+                                bg='white'
+                                borderRadius='lg'
+                                boxShadow='md'
+                                onDoubleClick={() =>
+                                    handleDoubleClick(project.name)
+                                } // Navigate on double-click
                             >
-                                <Flex justifyContent="space-between" alignItems="center">
-                                    <Heading as="h2" size="md" color="black.600" mb={2}>
+                                <Flex
+                                    justifyContent='space-between'
+                                    alignItems='center'
+                                >
+                                    <Heading
+                                        as='h2'
+                                        size='md'
+                                        color='black.600'
+                                        mb={2}
+                                    >
                                         {project.name}
                                     </Heading>
                                     <Flex gap={2}>
                                         <Box
-                                            as="button"
-                                            className="edit-board"
+                                            as='button'
+                                            className='edit-board'
                                             onClick={() => {
                                                 setEditingProject(project);
                                                 onEditOpen();
                                             }}
                                         >
-                                        <Box fontSize="30px">
-                                            <Icon name="bx-edit" />
+                                            <Box fontSize='30px'>
+                                                <Icon name='bx-edit' />
+                                            </Box>
                                         </Box>
+                                        <Box
+                                            as='button'
+                                            className='delete-board'
+                                            onClick={() =>
+                                                deleteProject(project.id)
+                                            } // Delete project */}
+                                        >
+                                            <Icon name='bx-trash' />
                                         </Box>
-                                    <Box
-                                    as="button"
-                                        className="delete-board"
-                                        onClick={() => deleteProject(project.id)} // Delete project */}
-                                    >
-                                      <Icon name="bx-trash" /> 
-                                    </Box>
                                     </Flex>
                                 </Flex>
-                                <Text fontSize="sm" color="gray.700">
-                                    {project.description || "No description provided."}
+                                <Text fontSize='sm' color='gray.700'>
+                                    {project.description ||
+                                        'No description provided.'}
                                 </Text>
                             </Box>
                         ))
                     ) : (
-                        <Box gridColumn="span 4" textAlign="center" color="gray.500" fontSize="lg">
-                            No projects yet! Click "Add New Project" to get started.
+                        <Box
+                            gridColumn='span 4'
+                            textAlign='center'
+                            color='gray.500'
+                            fontSize='lg'
+                        >
+                            No projects yet! Click "Add New Project" to get
+                            started.
                         </Box>
                     )}
                 </Box>
 
                 {/* Button to open the modal */}
-                <Box mt={8} textAlign="center">
-                    <Button colorScheme="white" size="lg" bg="black"onClick={onOpen}>
+                <Box mt={8} textAlign='center'>
+                    <Button
+                        colorScheme='white'
+                        size='lg'
+                        bg='black'
+                        onClick={onOpen}
+                    >
                         Add New Project
                     </Button>
                 </Box>
@@ -132,30 +207,39 @@ const ProjectList = () => {
                         <ModalCloseButton />
                         <ModalBody>
                             <Input
-                                placeholder="Project Name"
+                                placeholder='Project Name'
                                 value={newProjectName}
-                                onChange={(e) => setNewProjectName(e.target.value)}
+                                onChange={(e) =>
+                                    setNewProjectName(e.target.value)
+                                }
                                 mb={4}
-                                size="lg"
+                                size='lg'
                             />
                             <Textarea
-                                placeholder="Project Description (optional)"
+                                placeholder='Project Description (optional)'
                                 value={newProjectDescription}
-                                onChange={(e) => setNewProjectDescription(e.target.value)}
-                                size="lg"
+                                onChange={(e) =>
+                                    setNewProjectDescription(e.target.value)
+                                }
+                                size='lg'
                             />
                         </ModalBody>
                         <ModalFooter>
-                            <Button colorScheme="white"  bg="black" mr={3} onClick={addProject}>
+                            <Button
+                                colorScheme='white'
+                                bg='black'
+                                mr={3}
+                                onClick={addProject}
+                            >
                                 Add Project
                             </Button>
-                            <Button variant="ghost" onClick={onClose}>
+                            <Button variant='ghost' onClick={onClose}>
                                 Cancel
                             </Button>
                         </ModalFooter>
                     </ModalContent>
                 </Modal>
-                
+
                 {/* Modal for editing a project */}
                 {editingProject && (
                     <Modal isOpen={isEditOpen} onClose={onEditClose} isCentered>
@@ -165,16 +249,19 @@ const ProjectList = () => {
                             <ModalCloseButton />
                             <ModalBody>
                                 <Input
-                                    placeholder="Project Name"
+                                    placeholder='Project Name'
                                     value={editingProject.name}
                                     onChange={(e) =>
-                                        setEditingProject({ ...editingProject, name: e.target.value })
+                                        setEditingProject({
+                                            ...editingProject,
+                                            name: e.target.value,
+                                        })
                                     }
                                     mb={4}
-                                    size="lg"
+                                    size='lg'
                                 />
                                 <Textarea
-                                    placeholder="Project Description (optional)"
+                                    placeholder='Project Description (optional)'
                                     value={editingProject.description || ''}
                                     onChange={(e) =>
                                         setEditingProject({
@@ -182,14 +269,19 @@ const ProjectList = () => {
                                             description: e.target.value,
                                         })
                                     }
-                                    size="lg"
+                                    size='lg'
                                 />
                             </ModalBody>
                             <ModalFooter>
-                                <Button colorScheme="white" bg="black" mr={3} onClick={editProject}>
+                                <Button
+                                    colorScheme='white'
+                                    bg='black'
+                                    mr={3}
+                                    onClick={editProject}
+                                >
                                     Save Changes
                                 </Button>
-                                <Button variant="ghost" onClick={onEditClose}>
+                                <Button variant='ghost' onClick={onEditClose}>
                                     Cancel
                                 </Button>
                             </ModalFooter>
